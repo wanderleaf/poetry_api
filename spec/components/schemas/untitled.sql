61ae90efd7f5e98a12812781a03409889ce2fd01
dyliu <andrewliu@gmail.com>
2019-09-19 12:31:37 +0800

    add build stat interface

--- spec/components/schemas/untitled.sql
+++
@@ -1,8 +0,0 @@
-# qvm
-db.template_service.update({"service_name":{"$in":["cms-server"]}},{$set:{"product_name":"qvm"}},true,true)
-# pay
-db.template_service.update({"service_name":{"$in":["priced", "priceshimd", "walletexd", "walletbizd", "productd", "mock-bi", "dictd-v4", "measured-v4", "measureproxyd-v4", "priced-v4", "walletd-v4"]}},{$set:{"product_name":"pay"}},true,true)
-# stream-benchmark
-db.template_service.update({"service_name":{"$in":["stream-benchmark"]}},{$set:{"product_name":"stream-benchmark"}},true,true)
-# ue
-db.template_service.update({"service_name":{"$in":["portal-kodo-ue", "icecream"]}},{$set:{"product_name":"ue"}},true,true)
